84a1d9f737350f564d83822ecc8fd8fd73e14fb7
benjaminviaud <bv.notifs@gmail.com>
2025-03-25 17:03:18 +0100

    Article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    public array $articles = [
+        [
+            'id' => 1,
+            'title' => 'Mon 1er article',
+            'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+        ],
+        [
+            'id' => 2,
+            'title' => 'Mon 2nd article',
+            'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+        ],
+        [
+            'id' => 3,
+            'title' => 'Mon 3ème article',
+            'description' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."
+        ],
+    ];
+
+    #[Route('/articles', name: 'article.index')]
+    public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $this->articles
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'article.show')]
+    public function show(int $id): Response
+    {
+        $selected_article = [];
+
+        foreach($this->articles as $article){
+            if($article['id'] === $id) $selected_article = $article;
+        }
+
+        return $this->render('article/show.html.twig', [
+            'article' => $selected_article
+        ]);
+    }
+}
